8ec2b0ef80840ca7646e684fb1fca30c93ccf786
Danielk10 <danielpdiamon@gmail.com>
2022-01-04 15:56:17 -0430

    Primera version

---
+++ android/src/com/diamon/prueba/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.diamon.prueba;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.diamon.prueba.PruebasLibGdx10;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PruebasLibGdx10(), config);
+	}
+}
